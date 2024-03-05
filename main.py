26d28e226f74c90b2f70b90ded9c833bfa5243a3
IDGS-801-21000423 <almeidaangel474@gmail.com>
2024-03-04 18:51:16 -0600

    JARA 04/03/24 Modificar y Eliminar Flask_BD

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # --------------------------------------------------------------
-from flask import Flask, render_template, request     # Importar flask, render template
+from flask import Flask, render_template, request, url_for, redirect     # Importar flask, render template
 import forms                                          # Importar archivo forms
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
@@ -65,12 +65,58 @@ def index():
   return render_template("index.html", form=create_form)
 
 # Mostrar Alumnos
+
 @app.route("/ABC_Completo", methods=['GET', 'POST'])
 def ABCompleto():
   alum_form = forms.UserForm2(request.form)
   alumno = Alumnos.query.all()
   
   return render_template("ABC_Completo.html", alumno=alumno)
+
+@app.route('/eliminar', methods=['GET', 'POST'])
+def eliminar():
+  create_form = forms.UserForm2(request.form)
+  if request.method == 'GET':
+    id = request.args.get('id')
+    # select * from alumnos where id == id
+    alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+    create_form.id.data = request.args.get('id')
+    create_form.nombre.data = alum1.nombre
+    create_form.apaterno.data = alum1.apaterno
+    create_form.email.data = alum1.email
+  
+  if request.method == 'POST':
+    id = create_form.id.data
+    alum = Alumnos.query.get(id)
+    # delete from alumnos where id = id
+    db.session.delete(alum)
+    db.session.commit()
+    return redirect(url_for('ABCompleto'))
+  return render_template('eliminar.html', form=create_form)
+
+@app.route('/modificar', methods=['GET', 'POST'])
+def modificar():
+  create_form = forms.UserForm2(request.form)
+  if request.method == 'GET':
+    id = request.args.get('id')
+    # select * from alumnos where id == id
+    alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+    create_form.id.data = request.args.get('id')
+    create_form.nombre.data = alum1.nombre
+    create_form.apaterno.data = alum1.apaterno
+    create_form.email.data = alum1.email
+  
+  if request.method == 'POST':
+    id = create_form.id.data
+    alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+    alum1.nombre = create_form.nombre.data
+    alum1.apaterno = create_form.apaterno.data
+    alum1.email = create_form.email.data 
+    # delete from alumnos where id = id
+    db.session.add(alum1)
+    db.session.commit()
+    return redirect(url_for('ABCompleto'))
+  return render_template('modificar.html', form=create_form)
 
 if __name__ =="__main__":
   csrf.init_app(app)
